dd5c03ed54f7d5ef38bf868982bb787f86640762
Anders Hammervold <anders@hammervold.com>
2010-08-19 17:26:15 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Minepenger::Application.config.secret_token = '550ba3a094c8559d8f9df6cb95eef9b788bb5f2a56356de03b7c34c9be620e969db109b3be518be75d59151c8f27bb73cfdcb45125948d3e34095a1726afb7e5'
